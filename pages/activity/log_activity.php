a7c4830a1d46591744baa70d9aeb273fa031014f
angga2000 <anggaramadan091@gmail.com>
2024-03-06 14:51:41 +0700

    add logo & icon

--- pages/activity/log_activity.php
+++ pages/activity/log_activity.php
@@ -43,6 +43,7 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log Aktivitas</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../../assets/img/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Gaya dari navbar */
